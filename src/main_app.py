4a9e04cdf1cbc91e71c619e6fda296d9b95fac34
Samoppakiks <87269875+Samoppakiks@users.noreply.github.com>
2023-05-09 21:16:27 +0530

    cleaned up requirements.txt

--- src/main_app.py
+++ src/main_app.py
@@ -5,13 +5,18 @@ import pickle
 from .embedchat import chatgpt_get_response
 import requests
 
+from .config import pinecone_api_key, pinecone_environment
+
+"""pinecone_api_key = os.environ.get("PINECONE_API_KEY")
+pinecone_environment = os.environ.get("PINECONE_ENV")"""
+
 
 def get_pinecone_namespaces():
     url = "https://rajgov-f44d42f.svc.northamerica-northeast1-gcp.pinecone.io/describe_index_stats"
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
-        "Api-Key": "6d0b2d52-99bc-4de5-bec0-0c157c66ecbd"
+        "Api-Key": pinecone_api_key,
     }
     response = requests.get(url, headers=headers)  # Update the method to GET
     print("Server response:", response.text)
@@ -35,11 +40,19 @@ def get_extracted_text_path(pdf_path):
 
 
 def read_extracted_text(txt_file_path):
-    with open(txt_file_path, 'r', encoding='utf-8') as file:
+    with open(txt_file_path, "r", encoding="utf-8") as file:
         return file.read()
 
 
-def process_pdf_and_query(query, search_scope='current_file', pdf_path=None, namespace=None, department=None, type_of_document=None, year=None):
+def process_pdf_and_query(
+    query,
+    search_scope="current_file",
+    pdf_path=None,
+    namespace=None,
+    department=None,
+    type_of_document=None,
+    year=None,
+):
     create_directory_if_not_exists("./embeddings")
 
     if pdf_path:
@@ -59,12 +72,20 @@ def process_pdf_and_query(query, search_scope='current_file', pdf_path=None, nam
         extracted_text = ""
 
     answer, search_results = process_extracted_text(
-        query, extracted_text, pdf_path, search_scope, department=department, type_of_document=type_of_document, year=year, namespace=namespace)
+        query,
+        extracted_text,
+        pdf_path,
+        search_scope,
+        department=department,
+        type_of_document=type_of_document,
+        year=year,
+        namespace=namespace,
+    )
     return answer, search_results
 
 
 if __name__ == "__main__":
-    pdf_path = '../PDF/File_Udaan_pads.pdf'
-    query = 'What is Rajivika producing for Udaan scheme?'
+    pdf_path = "../PDF/File_Udaan_pads.pdf"
+    query = "What is Rajivika producing for Udaan scheme?"
     final = process_pdf_and_query(pdf_path, query)
     print(final)
