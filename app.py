e9d3d08fcb0607edeca95b1537fb00b8a9f4a7aa
Samoppakiks <87269875+Samoppakiks@users.noreply.github.com>
2023-05-11 21:39:53 +0530

    adding eventlet for websocket issue

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import eventlet
+
+eventlet.monkey_patch()
 import os
 from flask import Flask, request, render_template, redirect, url_for, jsonify, session
 from flask_socketio import SocketIO, emit
